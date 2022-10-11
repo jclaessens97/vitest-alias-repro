d7360c6c12dbfbaa5b18d5093bb4f25cc00718bd
Jeroen Claessens <jclaessens97@hotmail.com>
2022-10-11 12:11:10 +0200

    chore: update repro

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -1,14 +1,20 @@
 import path from 'path';
 import { defineConfig } from 'vitest/config';
 
+console.log('vitest', __dirname);
+
 export default defineConfig({
   test: {
     // globals: true,
     // deps: { inline: ['@fastify/autoload'] },
+    deps: {
+      registerNodeLoader: true,
+      // inline: true,
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@/': path.resolve(__dirname, './src/'),
     },
   },
 });
